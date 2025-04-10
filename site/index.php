588fe9c794eeac9b5f8e97e3bd4f75074289c486
Lucas <lc3197@gmail.com>
2025-04-10 17:15:35 -0300

    versao sem style

---
+++ site/index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Minha Loja</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <style type = "text/css">
+            .navbar
+            {
+                margin-bottom: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <?php include 'nav.php' ?>
+        <?php include 'cabecalho.html' ?>
+
+        <div class="conteiner-fluid">
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img-responsive" style="width:100%">
+                    <div><h1>Nome do livro</h1></div>
+                    <div><h4>R$45,00 </h4></div>
+                    
+                </div>
+
+                <div class="col-sm-3">
+                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img-responsive" style="width:100%">
+                    <div><h1>Nome do livro</h1></div>
+                    <div><h4>R$45,00 </h4></div>
+                    
+                </div>
+
+                <div class="col-sm-3">
+                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img-responsive" style="width:100%">
+                    <div><h1>Nome do livro</h1></div>
+                    <div><h4>R$45,00 </h4></div>
+                    
+                </div>
+
+                <div class="col-sm-3">
+                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img  -responsive" style="width:100%">
+                    <div><h1>Nome do livro</h1></div>
+                    <div><h4>R$45,00 </h4></div>      
+                </div>
+
+                <div class="col-sm-3">
+                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img  -responsive" style="width:100%">
+                    <div><h1>Nome do livro</h1></div>
+                    <div><h4>R$45,00 </h4></div>      
+                </div>
+
+                <div class="col-sm-3">
+                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img  -responsive" style="width:100%">
+                    <div><h1>Nome do livro</h1></div>
+                    <div><h4>R$45,00 </h4></div>      
+                </div>
+
+                <div class="col-sm-3">
+                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img  -responsive" style="width:100%">
+                    <div><h1>Nome do livro</h1></div>
+                    <div><h4>R$45,00 </h4></div>      
+                </div>
+
+                <div class="col-sm-3">
+                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img  -responsive" style="width:100%">
+                    <div><h1>Nome do livro</h1></div>
+                    <div><h4>R$45,00 </h4></div>      
+
+                    <div class="text-center">
+                        <button class="btn btn-lg btn-block">
+
+                        </button>
+                    </div>
+                </div>
+                  
+            <div> <!--fechamento da class row-->
+        </div> <!--fechamento do conteiner fluid-->
+
+        <?php 
+            include 'rodape.html'; 
+        ?>
+    </body>
+</html>
---
+++ site/nav.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-inverse">
+<div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Livraria</a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li><a href="#">Home <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Lançamentos </a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gêneros <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Terror</a></li>
+                    <li><a href="#">Sci-Fi</a></li>
+                    <li><a href="#">Romance</a></li>
+                    <li><a href="#">Biografia</a></li>
+                    <li><a href="#">Estudos</a></li>
+                </ul>
+            </li>
+        </ul>
+        <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Pesquisar">
+            </div>
+            <button type="submit" class="btn btn-default">Buscar</button>
+        </form>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Contato</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"> Log-in</a> </li>
+        </ul>
+    </div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+</nav>
