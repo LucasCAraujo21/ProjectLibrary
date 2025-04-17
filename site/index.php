41f27caefc417a1a1028eeecef33ae4be6331da0
Lucas <lc3197@gmail.com>
2025-04-17 16:53:09 -0300

    versao style incompleta

--- site/index.php
+++ site/index.php
@@ -14,6 +14,9 @@
         <!-- Latest compiled JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+        <title>Livraria</title>
+        <link rel="stylesheet" href="style.css">
+
         <style type = "text/css">
             .navbar
             {
@@ -22,73 +25,96 @@
         </style>
     </head>
     <body>
-        <?php include 'nav.php' ?>
-        <?php include 'cabecalho.html' ?>
+    <?php ?>
 
-        <div class="conteiner-fluid">
-            <div class="row">
-                <div class="col-sm-3">
-                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img-responsive" style="width:100%">
-                    <div><h1>Nome do livro</h1></div>
-                    <div><h4>R$45,00 </h4></div>
-                    
+    <?php include 'nav.php' ?>
+    <?php include 'cabecalho.html' ?>
+<main>
+
+    <!-- Slides Banners de Promoção -->
+    <section id="banners-promocionais" class="carousel slide" data-bs-ride="true" data-ride="carousel">
+
+        <!-- Imagens -->
+        <article class="carousel-inner">
+            <figure class="carousel-item active">
+                <img src="./assets/images/1.png" class="d-block w-100" alt="Banner Promocional">
+            </figure>
+            <figure class="carousel-item">
+                <img src="./assets/images/2.png" class="d-block w-100" alt="Banner Promocional 2">
+            </figure>
+            <figure class="carousel-item">
+                <img src="./assets/images/3.png" class="d-block w-100"
+                    alt="Banner Promocional 3.png">
+            </figure>
+        </article>
+
+        <!-- Botão Anterior -->
+        <a class="carousel-control-prev" type="button" href="#banners-promocionais" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden"></span>
+        </a>
+
+        <!-- Botão Proximo -->
+        <a class="carousel-control-next" type="button" href="#banners-promocionais" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden"></span>
+        </a>
+    </section>
+    <!-- Fim Slides Banners de Promoção -->
+
+    <!-- Banners de Promoção com 6 colunas -->
+    <section class="container banners-promocionais-statico">
+        <!-- Row -->
+        <section class="row ">
+            <!-- Banner -->
+            <article class="col-md-6">
+
+                <div class="banners-promocionais-statico-12x d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
+                        class="bi bi-credit-card" viewBox="0 0 16 16">
+                        <path
+                            d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z" />
+                        <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z" />
+                    </svg>
+                    <p>Parcele em até<br>
+                        <strong> 12x sem juros</strong>
+                    </p>
                 </div>
 
-                <div class="col-sm-3">
-                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img-responsive" style="width:100%">
-                    <div><h1>Nome do livro</h1></div>
-                    <div><h4>R$45,00 </h4></div>
-                    
+            </article>
+
+            <!-- Banner -->
+            <article class="col-md-6">
+                <div class="banners-promocionais-statico-todo-br d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
+                        class="bi bi-cart-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
+                    <p>
+                        Entrega para <br>
+                        <strong>todo Brasil</strong>
+                    </p>
+
                 </div>
+            </article>
 
-                <div class="col-sm-3">
-                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img-responsive" style="width:100%">
-                    <div><h1>Nome do livro</h1></div>
-                    <div><h4>R$45,00 </h4></div>
-                    
-                </div>
+        </section>
+        <!-- Fim Row -->
+    </section>
+    <!-- Fim Banners de Promoção com 6 colunas -->
 
-                <div class="col-sm-3">
-                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img  -responsive" style="width:100%">
-                    <div><h1>Nome do livro</h1></div>
-                    <div><h4>R$45,00 </h4></div>      
-                </div>
+    <!-- Container Produtos -->
+    <section class="container produtos">
+       
 
-                <div class="col-sm-3">
-                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img  -responsive" style="width:100%">
-                    <div><h1>Nome do livro</h1></div>
-                    <div><h4>R$45,00 </h4></div>      
-                </div>
+    </section >
+    <!-- Fim Container Produtos -->
 
-                <div class="col-sm-3">
-                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img  -responsive" style="width:100%">
-                    <div><h1>Nome do livro</h1></div>
-                    <div><h4>R$45,00 </h4></div>      
-                </div>
+</main>
 
-                <div class="col-sm-3">
-                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img  -responsive" style="width:100%">
-                    <div><h1>Nome do livro</h1></div>
-                    <div><h4>R$45,00 </h4></div>      
-                </div>
 
-                <div class="col-sm-3">
-                    <img src="https://i.pinimg.com/564x/7a/4c/bc/7a4cbcd3767b19ccc345b21048b7798a.jpg" class="img  -responsive" style="width:100%">
-                    <div><h1>Nome do livro</h1></div>
-                    <div><h4>R$45,00 </h4></div>      
+<?php include 'rodape.php' ?>
 
-                    <div class="text-center">
-                        <button class="btn btn-lg btn-block">
-
-                        </button>
-                    </div>
-                </div>
-                  
-            <div> <!--fechamento da class row-->
-        </div> <!--fechamento do conteiner fluid-->
-
-        <?php 
-            include 'rodape.html'; 
-        ?>
-    </body>
+</body>
 </html>
--- site/nav.php
+++ site/nav.php
@@ -1,41 +1,136 @@
-<nav class="navbar navbar-inverse">
-<div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Livraria</a>
-    </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-            <li><a href="#">Home <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Lançamentos </a></li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gêneros <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="#">Terror</a></li>
-                    <li><a href="#">Sci-Fi</a></li>
-                    <li><a href="#">Romance</a></li>
-                    <li><a href="#">Biografia</a></li>
-                    <li><a href="#">Estudos</a></li>
+<!-- Inicio Header -->
+<header class="container-fluid">
+    <!-- Container -->
+    <section class="container">
+        <!-- Row - colunas -->
+        <article class="row d-flex align-items-center">
+            <!-- "Logo" -->
+            <a href="index.php" class="col-md-3  d-flex justify-content-center">
+                <img src="assets/images/logonobg.png" class="img-fluid logo" alt="Logo Livraria">
+            </a>
+            <!-- Buscar -->
+            <form action="index.php" class="col-md-6 d-flex align-items-center">
+                <input type="text" name="txtbuscar" placeholder="Pesquisar Instrumentos aqui">
+
+                <button class="d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-search" viewBox="0 0 16 16">
+                        <path
+                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                </button>
+            </form>
+
+            <!-- login funcionario -->
+            <ul class="col-md-3 nav d-flex align-items-center justify-content-around">
+                <li class="nav-item">
+                    <a href="#">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                        </svg>
+                        Entrar
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" id="cart-icon">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" class="bi bi-cart-fill " >
+                            <path
+                                d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                
+                        </svg>  
+                        Carrinho
+                    </a>
+                    <!-- Carrinho -->
+                    <div class="cart" style="z-index:1000">
+                        <h2 class="cart-title">Seu Carrinho</h2>
+                        <div class="cart-content">
+                            <div class="cart-box">
+                                <img src="./assets/images/" class="img-fluid cart-img" alt="Guitarras">
+                                <div class="detail-box">
+                                    <div class="cart-product-title"><?php echo $exibe['nome_prod'];?></div>
+                                    <div class="cart-price">R$<?php echo $exibe['valor'];?></div>
+                                    <div class="cart-price-title">Quantidade: <?php echo $exibe['quant_prod'];?></div>
+                                </div>
+                                    
+                                <!--Remover-->
+                                <a href="Cart0.php?id=<?php echo $exibe['id_prod']; ?>">
+                                <i class="bx bxs-trash-alt cart-remove"></i>
+                                </a>
+                            </div>   
+                        </div>
+
+                            <!--Fechar Carrinho-->
+                            <i class='bx bx-x' id="close-cart"></i>
+                            
+                            <div class="total">
+                                <div class="total-title">Total</div>
+                                <div class="total-price">R$<?php echo $exibeTotal['valor'];?></div>
+                            </div>
+                        <a href="carrinho.php">   
+                            <button type="button" class="btn btn-success col-md-12 finaliza">Finalizar compra</button>
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </article>
+        <!-- Fim Row -->
+    </section>
+    <!-- Fim Container -->
+</header>
+<!-- Fim do Header -->
+
+<!-- Inicio Menu do Site -->
+<nav class="container-fluid nav-produtos ">
+    <!-- Container -->
+    <section class="container">
+        <!-- Menu -->
+        <ul class="nav">
+            <!-- Lista de itens -->
+            <li class="col-xl-2 col-md-12 nav-item d-flex align-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
+                    class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                </svg>
+                Livros
+                <!-- SubMenu -->
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a href="#guitarra">
+                            Sci-fi
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#violões">
+                            Terror
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#bateria">
+                            RPG
+                        </a>
+
+                    </li>
+                    <li class="nav-item">
+                        <a href="#piano">
+                            Estudos
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#violinos">
+                            Hqs
+                        </a>
+                    </li>
+
                 </ul>
+                <!-- Fim SubMenu -->
             </li>
+            <!-- Lista de intens -->
         </ul>
-        <form class="navbar-form navbar-left" role="search">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Pesquisar">
-            </div>
-            <button type="submit" class="btn btn-default">Buscar</button>
-        </form>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Contato</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"> Log-in</a> </li>
-        </ul>
-    </div><!-- /.navbar-collapse -->
-</div><!-- /.container-fluid -->
+    </section>
 </nav>
+<!-- Fim Menu do Site -->
+
